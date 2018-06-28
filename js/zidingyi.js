c19ee918177f3e42d02537ee4263e59f857c14ac
18603779064 <36759145+18603779064@users.noreply.github.com>
2018-06-28 23:15:22 +0800

    Site updated: 2018-06-28 23:15:22

--- js/zidingyi.js
+++ js/zidingyi.js
@@ -1,8 +1,27 @@
-$(function() { 
-  var heigth = document.documentElement.clientHeight || document.body.clientHeight;
-  alert(heigth);
-	var mixHeight = heigth-80;
-    $("#body_container").css('min-height',mixHeight);
-    //更改页面高度为  可视高度减去底部高度
-}
-
+$(document).ready(function(){
+	$(window).scroll(function(){/**设置顶部固定      */  
+		var scrollPos = $(window).scrollTop();   //获取滚动条滚动的高度
+		if(scrollPos >= 120){   //70:头部高度-菜单栏的高度
+			$('#nav1').addClass('fixedNav')
+		}else{
+			$('#nav1').removeClass('fixedNav')
+		}			 
+	})
+	setHeight();
+})
+	/**设置底部固定      */  	
+	function setHeight(){
+		
+		var footerHeight = $('#123').height();
+		var bodyHeight = $('.body_container').height()+footerHeight;
+		var height = document.documentElement.clientHeight;
+		if(bodyHeight<height){
+			var minHeight = height-footerHeight-5;
+			minHeight=minHeight+'px';
+			//alert('底部高度:'+footerHeight+';body宽度:'+height+';修改后的宽度:'+minHeight);
+			$('.body_container').css('minHeight',minHeight);
+		}
+	}
+	window.onresize=function(){
+		setHeight();
+	}
