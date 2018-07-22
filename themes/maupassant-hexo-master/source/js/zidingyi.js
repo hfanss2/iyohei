64231ad4962ac12f39cc6aba1dbd9e92089e2299
18603779064 <36759145+18603779064@users.noreply.github.com>
2018-07-22 17:50:07 +0800

    1111

--- themes/maupassant-hexo-master/source/js/zidingyi.js
+++ themes/maupassant-hexo-master/source/js/zidingyi.js
@@ -1,31 +1,30 @@
-
+//页面加载完成执行的方法
 $(document).ready(function(){
-	
 	setHeight();
 })	
 	
-	/**设置底部固定      */  	
-	function setHeight(){
-		var footerHeight = $('#footer-000').height();
-		var headerHeight = $('#header-02').height();
-		var height = document.documentElement.clientHeight;
-		var minHeight = height-footerHeight;
-		minHeight=minHeight+'px';
-		//alert('底部高度:'+footerHeight+';body宽度:'+height+';修改后的宽度:'+minHeight);
-		$('.body_container').css('minHeight',minHeight);
-	}
-	/*  显示搜索框 */
-	function showSerch(){
-		$(".search-total").css('display', 'initial');
-		document.getElementById('local-search-input').focus(); 
-	}
-	/*  隐藏搜索框 */
-	function outSerch(){
-		$(".search-total").css('display', 'none');
-	}
-	
-	//浏览器大小改变时
-	window.onresize=function(){
-		setHeight();
-	}
+/**设置底部固定      */  	
+function setHeight(){
+	var footerHeight = $('#footer-000').height();
+	var headerHeight = $('#header-02').height();
+	var height = document.documentElement.clientHeight;
+	var minHeight = height-footerHeight;
+	minHeight=minHeight+'px';
+	//alert('底部高度:'+footerHeight+';body宽度:'+height+';修改后的宽度:'+minHeight);
+	$('.body_container').css('minHeight',minHeight);
+}
+/*  显示搜索框 */
+function showSerch(){
+	$(".search-total").css('display', 'initial');
+	document.getElementById('local-search-input').focus(); 
+}
+/*  隐藏搜索框 */
+function outSerch(){
+	$(".search-total").css('display', 'none');
+}
+
+//浏览器大小改变时
+window.onresize=function(){
+	setHeight();
+}
 
